88d413f60d4abaea1796e31a80b1c80bac561e1f
vagrant <vagrant@localhost.localdomain>
2020-05-23 13:17:35 +0000

    [Finish] skill up 4~6

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,29 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
- context "データが正しく保存される" do
-        before do
-            @post = Post.new
-            @post.title = "Hello WebCamp"
-            @post.content = "今日も晴れです。"
-            @post.save
-        end
-        it "全て入力してあるので保存される" do
-            expect(@post).to be_valid
-        end
+  context "データが正しく保存される" do
+    before do
+      @post = Post.new
+      @post.title = "Hello WebCamp"
+      @post.content = "今日も晴れです。"
+      @post.save
     end
-    # ここから追加
-    context "データが正しく保存されない" do
-        before do
-            @post = Post.new
-            @post.title = ""
-            @post.content = "今日も晴れです。"
-            @post.save
-        end
-        it "titleが入力されていないので保存されない" do
-            expect(@post).to be_invalid
-            expect(@post.errors[:title]).to include("can't be blank")
-        end
+
+    it "全て入力してあるので保存される" do
+      expect(@post).to be_valid
     end
-    # ここまで追加
+  end
+  # ここから追加
+
+  context "データが正しく保存されない" do
+    before do
+      @post = Post.new
+      @post.title = ""
+      @post.content = "今日も晴れです。"
+      @post.save
+    end
+
+    it "titleが入力されていないので保存されない" do
+      expect(@post).to be_invalid
+      expect(@post.errors[:title]).to include("can't be blank")
+    end
+  end
+  # ここまで追加
 end
